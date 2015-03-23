8a9148f4f18b1c4ca684dd161b036085277a7110
Steven Sarasin <tutoringsteve@gmail.com>
2015-03-23 07:13:05 -0400

    Added tests to confirm function of size() for sizes larger than 0 and 1, simulating induction by asserting each increment of the size corresponds with a correct return value of size().

--- test_adts.py
+++ test_adts.py
@@ -5,7 +5,6 @@ import adts
 
 
 class ListQueueTest(unittest.TestCase):
-
     def setUp(self):
         self.q = adts.ListQueue()
 
@@ -27,6 +26,12 @@ class ListQueueTest(unittest.TestCase):
         self.assertTrue(self.q.size() > 0, "q contains 'first' but has non-positive size()")
         self.assertFalse(self.q.is_empty(), "q has positive size but is_empty() returns true")
 
+    def test_queue_size_increased_by_one_when_cargo_pushed_to_queue_of_size_k(self):
+        # decreasing range(a, b) upper bound does not improve test speed appreciably
+        for i in range(1, 1000):
+            self.q.enqueue(i)
+            self.assertEqual(i, self.q.size())
+
     def test_dequeue_on_empty_queue_returns_None(self):
         self.assertEqual(None, self.q.dequeue())
 
@@ -46,7 +51,6 @@ class ListQueueTest(unittest.TestCase):
 
 
 class QueueTest(unittest.TestCase):
-
     def setUp(self):
         self.q = adts.Queue()
 
@@ -68,6 +72,12 @@ class QueueTest(unittest.TestCase):
         self.assertTrue(self.q.size() > 0, "q contains 'first' but has non-positive size()")
         self.assertFalse(self.q.is_empty(), "q has positive size but is_empty() returns true")
 
+    def test_queue_size_increased_by_one_when_cargo_pushed_to_queue_of_size_k(self):
+        # decreasing range(a, b) upper bound does not improve test speed appreciably
+        for i in range(1, 1000):
+            self.q.enqueue(i)
+            self.assertEqual(i, self.q.size())
+
     def test_dequeue_on_empty_queue_returns_None(self):
         self.assertEqual(None, self.q.dequeue())
 
@@ -84,6 +94,68 @@ class QueueTest(unittest.TestCase):
         for cargo in elements:
             self.q.enqueue(cargo)
         self.assertEqual(str(elements), str(self.q))
+
+
+class StackTest(unittest.TestCase):
+    def setUp(self):
+        self.s = adts.Stack()
+
+    def tearDown(self):
+        self.s = None
+
+    def test_stack_is_empty_on_creation(self):
+        self.assertTrue(self.s.is_empty())
+
+    def test_stack_has_size_zero_on_creation(self):
+        self.assertEqual(0, self.s.size())
+
+    def test_stack_has_size_one_after_one_push(self):
+        self.s.push("first")
+        self.assertEqual(1, self.s.size())
+
+    def test_stack_is_not_empty_when_size_is_nonzero(self):
+        self.s.push("first")
+        self.assertTrue(self.s.size() > 0, "q contains 'first' but has non-positive size()")
+        self.assertFalse(self.s.is_empty(), "q has positive size but is_empty() returns true")
+
+    def test_stack_size_increased_by_one_when_cargo_pushed_to_stack_of_size_k(self):
+        # decreasing range(a, b) upper bound does not improve test speed appreciably
+        for i in range(1, 1000):
+            self.s.push(i)
+            self.assertEqual(i, self.s.size())
+
+    def test_pop_on_empty_stack_raises_RuntimeError(self):
+        # required so error raising pop() isn't run before the assertRaises
+        with self.assertRaises(RuntimeError):
+            self.s.pop()
+            
+    def test_peek_on_empty_stack_raises_RuntimeError(self):
+        # required so error raising peek() isn't run before the assertRaises
+        with self.assertRaises(RuntimeError):
+            self.s.peek()
+
+    def test_pop_reverses_push_order(self):
+        self.s.push("first")
+        self.s.push("second")
+        self.s.push("third")
+        self.assertEqual("third", self.s.pop(), "'third' not popped first")
+        self.assertEqual("second", self.s.pop(), "'second' not popped second")
+        self.assertEqual("first", self.s.pop(), "'first' not popped third")
+    def test_stack_converts_to_string_equivalent_to_string_conversion_of_a_list_of_all_its_elements(self):
+        elements = ['a', 'b', 1, 2]
+        for cargo in elements:
+            self.s.push(cargo)
+        self.assertEqual(str(elements), str(self.s))
+
+    def test_peek_returns_last_item_pushed_but_stack_unchanged(self):
+        elements = ['a', 'b', 1, 2]
+        for cargo in elements:
+            self.s.push(cargo)
+        self.assertEqual(len(elements), self.s.size(), "size of stack doesn't match number of elements pushed")
+        self.assertEqual(elements[-1], self.s.peek(), "not showing last element added")
+        self.assertEqual(len(elements), self.s.size(), "size of  stack changed by peek")
+        self.assertEqual(str(elements), str(self.s), "contents of stack changed by peek")
+
 
 if __name__ == "__main__":
     unittest.main()
